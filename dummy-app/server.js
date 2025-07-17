5112661b41d3f4f0db0eb289abc4f077f726f0eb
daniel <daniel.alm92@gmail.com>
2025-07-17 11:51:59 -0400

    first commit

---
+++ dummy-app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/api/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.post('/api/echo', (req, res) => {
+  res.json({ received: req.body });
+});
+
+const port = 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
